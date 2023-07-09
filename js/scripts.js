1c7b9d5c3b5987c036b4bb31c41177c41e0b19e8
ahmed ellatif <mokaworld2023@gmail.com>
2023-07-09 03:45:09 +0300

    update html

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,41 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
+
+$('.slick-slider').slick({
+    rtl:true,
+    autoplay:false,
+    nav:true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
