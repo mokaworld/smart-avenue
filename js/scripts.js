4877a6bb5f978aed620f099a63f566b13f11db23
ahmed ellatif <mokaworld2023@gmail.com>
2023-07-09 04:14:30 +0300

    update html

--- js/scripts.js
+++ js/scripts.js
@@ -32,8 +32,8 @@ $('.slick-slider').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 5,
+          slidesToScroll: 5,
           infinite: true,
           dots: true
         }
