cc8a17b9472ab663e5fa3ae0c745b246ad63f141
ahmed ellatif <mokaworld2023@gmail.com>
2023-07-10 22:33:26 +0300

    update html

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,4 @@
 
-//INIT OWL SLIDER 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    rtl:true,
-    autoplay:true,
-    autoplayTimeout:5000,
-    autoplayHoverPause:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-});
 //INIT SLICK SLIDER  
 $('.slick-slider').slick({
     rtl:true,
@@ -67,3 +46,4 @@ $('.bars').click( function(){
 $('.close').click( function(){
   $('.menu').fadeOut();
 });
+
